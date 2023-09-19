11eb56396811f13c9666b5e96840ffcb367cd268
Allanally <110419895+Allanally@users.noreply.github.com>
2023-09-19 04:01:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,11 +14,7 @@ require('dotenv').config();
 
 
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000"],
-  method: ["GET", "POST"],
-  credentials: true,
-}));
+app.use(cors());
 app.use(cookieParser())
 app.use(authRoutes);
 
